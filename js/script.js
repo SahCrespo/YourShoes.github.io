728e3fbe6ada0db144bc0ca41be723077d4b1580
Sabrina Crespo <sabrina_crespo@outlook.com>
2022-12-13 10:47:01 -0300

    Corrigido alguns erros no codigo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*const botao = document.querySelector('.borao')
+
+function mostrar() {
+  const menu = document.querySelector('.header-menu')
+
+  menu.classList.toggle('toggle')
+}
+
+botao.addEventListener('click', mostrar)*/
